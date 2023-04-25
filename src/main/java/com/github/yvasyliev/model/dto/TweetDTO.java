59136f5897cea8652b9139dc0be46768873e3d8c
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-04-25 13:38:54 +0300

    updated TweetDTO

--- src/main/java/com/github/yvasyliev/model/dto/TweetDTO.java
+++ src/main/java/com/github/yvasyliev/model/dto/TweetDTO.java
@@ -2,5 +2,5 @@ package com.github.yvasyliev.model.dto;
 
 import java.time.LocalDateTime;
 
-public record TweetDTO(Long id, String text, LocalDateTime createdAt, Long replies, Long likes) {
+public record TweetDTO(Long id, String text, LocalDateTime createdAt, Integer replies, Integer likes) {
 }
