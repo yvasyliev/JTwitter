21096e9cf596941d00a7c04fab0a73a8841b3beb
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-04-26 21:11:27 +0300

    fixed incorrect logic in SendEmailAspect#sendEmailConfirmation

--- src/main/java/com/github/yvasyliev/aop/SendEmailAspect.java
+++ src/main/java/com/github/yvasyliev/aop/SendEmailAspect.java
@@ -1,13 +1,11 @@
 package com.github.yvasyliev.aop;
 
 import com.github.yvasyliev.model.dto.TokenDTO;
-import com.github.yvasyliev.repository.TokenRepository;
 import com.github.yvasyliev.service.EmailSender;
+import com.github.yvasyliev.service.auth.TokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
@@ -15,10 +13,8 @@ import org.springframework.scheduling.annotation.Async;
 @Aspect
 @Configuration
 public class SendEmailAspect {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SendEmailAspect.class);
-
     @Autowired
-    private TokenRepository tokenRepository;
+    private TokenService tokenService;
 
     @Autowired
     private EmailSender emailSender;
@@ -26,11 +22,8 @@ public class SendEmailAspect {
     @Async
     @AfterReturning(value = "execution(* com.github.yvasyliev.controller.AuthenticationController.signUp(..)) && args(..,request)", returning = "tokenDTO")
     public void sendEmailConfirmation(HttpServletRequest request, TokenDTO tokenDTO) {
-        tokenRepository
-                .findById(tokenDTO.token())
-                .ifPresentOrElse(
-                        token -> emailSender.sendEmailConfirmation(request, token),
-                        () -> LOGGER.error("Token {} not found.", tokenDTO.token())
-                );
+        var user = tokenService.getById(tokenDTO.token()).getUser();
+        var emailToken = tokenService.createEmailToken(user);
+        emailSender.sendEmailConfirmation(request, emailToken);
     }
 }
